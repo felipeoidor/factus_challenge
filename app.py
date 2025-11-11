751cddb2627d51fd514434349d63d716bd3796ed
Pipe <pipe@felipeoidor.com>
2025-11-11 13:49:48 -0500

    feat: add municipality and department data

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from models import get_numbering_range
+from models import get_numbering_range, get_municipality
 from tokens import refreshed_access_token
 
 
@@ -8,8 +8,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    numbering_range = [
-        f"{x['id']}: {x['document']}" for x in get_numbering_range(refreshed_access_token)
+    data = get_municipality(refreshed_access_token)
+    numrange = get_numbering_range(refreshed_access_token)
+
+    numbering_range = [f"{x['id']}: {x['document']}" for x in numrange]
+
+    # department - municipality
+    locations = [
+        {"department": item["department"], "municipality": item["name"]}
+        for item in data
     ]
 
-    return render_template("index.html", numbering_range=numbering_range)
+    # select index.html (option)
+    department = sorted({item["department"] for item in locations})
+    municipality = [item["municipality"] for item in locations]
+
+    return render_template(
+        "index.html",
+        numbering_range=numbering_range,
+        department=department,
+        municipality=municipality,
+    )
