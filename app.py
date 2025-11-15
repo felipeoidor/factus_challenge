72290fd86ad567ef14da7dca30d023c5b1c8fe0c
Pipe <pipe@felipeoidor.com>
2025-11-15 15:47:02 -0500

    feat:add Flask route for invoice creation with dynamic catalog data

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-from flask import Flask, render_template
-from models import get_numbering_range, get_municipality
-from tokens import refreshed_access_token
+from flask import Flask, render_template, request
+from utils.json_loader import load_json
+from models import base_header, fetch_catalog_data
+from tokens import refreshed_token
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/crear-factura", methods=["GET", "POST"])
 def index():
-    data = get_municipality(refreshed_access_token)
-    numrange = get_numbering_range(refreshed_access_token)
+    endpoints = load_json(json_file="config/endpoints.json")
+    headers = base_header(refreshed_token)
 
-    numbering_range = [f"{x['id']}: {x['document']}" for x in numrange]
-
-    # department - municipality
-    locations = [
-        {"department": item["department"], "municipality": item["name"]}
-        for item in data
+    # Documents
+    documents = load_json(json_file="config/code.json", get="documents")
+    documents = [
+        f"{document['code']} - {document['document']}" for document in documents
     ]
+    if request.method == "POST":
+        document = request.form.get("document")
+        print(document)
 
-    # select index.html (option)
-    department = sorted({item["department"] for item in locations})
-    municipality = [item["municipality"] for item in locations]
+    # Numbering range
+    num_endpoint = endpoints.get("numbering_range").get("endpoint")
+    num_range = fetch_catalog_data(num_endpoint, headers).get("data")
+    num_range = [num["document"] for num in num_range.get("data")]
+    if request.method == "POST":
+        num = request.form.get("numbering-range")
+        print(num)
+
+    # Municipality
+    mun_endpoint = endpoints.get("municipality").get("endpoint")
+    mun_data = fetch_catalog_data(mun_endpoint, headers).get("data")
+    department = sorted({mun["department"] for mun in mun_data})
+    municipality = sorted({num["name"] for num in mun_data})
+    if request.method == "POST":
+        department_value = request.form.get("department")
+        municipality_value = request.form.get("municipality")
+        print(department_value)
+        print(municipality_value)
 
     return render_template(
         "index.html",
-        numbering_range=numbering_range,
+        documents=documents,
+        num_range=num_range,
         department=department,
         municipality=municipality,
     )
