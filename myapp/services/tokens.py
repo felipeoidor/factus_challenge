ade87ad00987a24a221082868450fd6bf68b6789
Pipe <pipe@felipeoidor.com>
2025-11-20 06:54:11 -0500

    refactor: move project files to root root to fix import issues

--- myapp/services/tokens.py
+++
@@ -1,6 +0,0 @@
-from myapp.services.api_client import fetch_auth_token, renew_token
-
-auth_token = fetch_auth_token()
-access_token = auth_token.get("access_token")
-refresh_token = auth_token.get("refresh_token")
-refreshed_token = renew_token(access_token, refresh_token).get("access_token")
